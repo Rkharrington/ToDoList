1d2c317857c9f90dc909eace65e9934e5564fb3b
Rkharrington <rachel.harrington2@gmail.com>
2023-11-09 22:51:10 -0600

    Convert to jQuery

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function newItem(){
+    // Adds new item to the list
+    let li= $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if(inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
+    
+    // Cross item out when user double clicks
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+    li.on('dblclick', function crossOut () {
+        li.toggleClass('strike');
+    });
+
+    // Add 'X' delete button
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    deleteButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem(){
+        li.addClass('delete')
+    }
+    
+    // Sort list
+    $('#list').sortable()
+}
